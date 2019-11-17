c0bac6d70c9022ba2e8b9b3a2aa141b922bdf0fa
r-makhnyk <roma.makhnyk@gmail.com>
2019-11-17 22:16:44 +0200

    - fixed carShop

--- VehicleShop.Shared/src/main/java/Services/Shop/Vehicle/CarShop.java
+++ VehicleShop.Shared/src/main/java/Services/Shop/Vehicle/CarShop.java
@@ -3,6 +3,7 @@ package Services.Shop.Vehicle;
 import Data.Entities.Vehicle.Car;
 import Data.Repository.IEntityRepository;
 import Services.Accounting.IAccountingService;
+import Services.Shop.Models.ProductListItem;
 import Services.Shop.ShopBase;
 
 public class CarShop
@@ -10,5 +11,10 @@ public class CarShop
 {
     public CarShop(IAccountingService accountingService, IEntityRepository<Integer, Car> carRepository) {
         super(accountingService, carRepository);
+    }
+
+    @Override
+    protected ProductListItem ToListItem(Car car) {
+        return new ProductListItem(car.get_key(), car.get_productType(),car.get_name(),car.get_price());
     }
 }
