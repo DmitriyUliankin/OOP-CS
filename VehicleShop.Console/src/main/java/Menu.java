fc7f2a5a64ba89f86072e727271d4de5e6e00576
Дмитрий <43218780+McMisle@users.noreply.github.com>
2019-11-17 21:15:46 +0200

    Add files via upload

---
+++ VehicleShop.Console/src/main/java/Menu.java
@@ -0,0 +1,20 @@
+public class Menu {
+
+    public void printMenu(){
+        System.out.println("1. Add vehicle");
+        System.out.println("2. Delete vehicle");
+        System.out.println("3. Show all vehicle");
+        System.out.println("4. Show all cars");
+        System.out.println("5. Show all Motorcycles");
+        System.out.println("6. Sale a vehicle to a customer");
+        System.out.println("7. Print sales by day");
+        System.out.println("8. Save to storage");
+        System.out.println("9. Get from storage");
+    }
+
+    public void addCar(){
+        System.out.println("What vehicle you want to add");
+        System.out.println("1. Car");
+        System.out.println("2. Motorcycle");
+    }
+}
